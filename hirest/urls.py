f67b3b841736762d3bff6e3909b927a3791b5d41
Parth Parmar <parth.parmar30803@gmail.com>
2023-04-08 17:47:31 +0530

    [ADD] added blog app

--- hirest/urls.py
+++ hirest/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('blog/',include('blogApp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
